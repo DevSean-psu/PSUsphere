0dafb094919ef396582a4386f96aa25e1dfd7a97
unknown <202480111@psu.palawan.edu.ph>
2026-08-24 20:10:49 +0800

    dynamic dashboard data

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,8 +1,18 @@
-from django.views.generic.list import ListView
-from studentorg.models import Organization
+from django.views.generic import ListView
+from studentorg.models import Organization, College, Program, Student
 
 
 class HomePageView(ListView):
     model = Organization
-    context_object_name = 'home'
+    context_object_name = 'organizations'
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['organization_count'] = Organization.objects.count()
+        context['college_count'] = College.objects.count()
+        context['program_count'] = Program.objects.count()
+        context['student_count'] = Student.objects.count()
+
+        return context
